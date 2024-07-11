8b64daec3c17600f5a3cf75eddd48b5af21bb05a
shaohan-n <shaohan.n@binance.us>
2024-07-11 10:57:59 +0800

    feature: fix error

--- playground/examples/borrow1.rs
+++ playground/examples/borrow1.rs
@@ -2,7 +2,7 @@ fn main() {
     let data = vec![1, 2, 3, 4, 5];
     let data1 = data;        // data is moved to data1
     println!("{}", sum(data1));
-    println!("{:?}", data1);
+    // println!("{:?}", data1);
     // println!("{}", sum(data));
 }
 
